745c84027b51fe6253c4a9fac0fc6bbb3498c098
Ambady <cssreedeep@gmail.com>
2023-05-05 22:03:52 +0530

    Part 2.2
    Showed also the sum of the exercises of the course.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const App = () => {
     {
       name: "State of a component",
       exercises: 14
+    },
+    {
+      name: "Redux",
+      exercises: 11
     }
   ]
 }
@@ -30,6 +34,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course}/>
     </div>
   );
 };
@@ -43,12 +48,22 @@ const Header = (props) => {
   )
 }
 
+const Total = (props) => {
+  return (
+    <div>
+      <h2>Number of exercises </h2>
+      <p>{props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises + props.course.parts[3].exercises}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
       <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
       <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
       <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      <Part part={props.course.parts[3].name} exercises={props.course.parts[3].exercises} />
     </div>
   );
 };
